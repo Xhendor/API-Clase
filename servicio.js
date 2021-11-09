482bf3099ea5b69a50d0aa641dfe7089033e60f1
Rosendo R. Sosa Canales <rosendorsc@gmail.com>
2021-11-08 18:23:32 -0800

    Router API TW y CORS

--- servicio.js
+++ servicio.js
@@ -3,12 +3,9 @@ const path= require('path')
 const fs=require('fs')
 const formidable= require('formidable')
 const session= require('express-session')
-const flash = require ('express-flash')
 const  routerIndex =require('./routes/index')
 const  routerProductos =require('./routes/productos')
-const routerAuth = require('./routes/auth')
 const cookieParser=require('cookie-parser')
-const router = require('./routes/index')
 const port = 3000
 var app = express();
 
@@ -16,24 +13,34 @@ var app = express();
 //npm install ejs
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-//npm install express-flash
-app.use(flash())
+
+const DOMINIO_PERMITIDO_CORS = "http://localhost:4200",
+  DIRECTORIO_FOTOS = path.join(__dirname, "fotos_productos"),
+  DIRECTORIO_DIST = path.join(__dirname, "dist"),
+  PUERTO = 3000;
+
+  app.use("/foto_producto", express.static(DIRECTORIO_FOTOS));
+  // Estático
+  app.use("/", express.static(DIRECTORIO_DIST));
+  
+  if (!fs.existsSync(DIRECTORIO_FOTOS)) {
+    fs.mkdirSync(DIRECTORIO_FOTOS);
+  }
+  app.use((req, res, next) => {
+    res.set("Access-Control-Allow-Credentials", "true");
+    res.set("Access-Control-Allow-Origin", DOMINIO_PERMITIDO_CORS);
+    res.set("Access-Control-Allow-Headers", "Content-Type");
+    res.set("Access-Control-Allow-Methods", "DELETE");
+    next();
+  });
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: false }));
-app.use(session({
-    secret: '123456abc',
-    resave:false,
-    saveUninitialized:true,
-    cookie:{maxAge: 60000}
-}))
-
 //npm install cookie-parser
 app.use(cookieParser());
 //Contenido estatico
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/',routerIndex)
 app.use('/productos',routerProductos)
-app.use('/auth',routerAuth)
 app.listen(port,function(){
 
     console.log('Example app ruta:localhost:'+port)
